66893b7e53947af0c426305264682012a32a5a1f
shivam kumar <shivamjmp2@gmail.com>
2026-04-17 01:15:52 +0530

    Refactor code structure for improved readability and maintainability

--- src/constants/index.js
+++ src/constants/index.js
@@ -6,7 +6,7 @@ export const projects = [
       "A clinical decision support platform that helps healthcare teams with faster, data-backed medical guidance.",
     href: "https://medicine-recommendation-system-4v7k.vercel.app/",
     image: "/assets/projects/MediAssist.png",
-    bgImage: "public/assets/backgrounds/bg1.jpg",
+    bgImage: "/assets/backgrounds/bg1.jpg",
     frameworks: [
       { id: 1, name: "React" },
       { id: 2, name: "TypeScript" },
@@ -22,7 +22,7 @@ export const projects = [
       "An education workflow system that automates class scheduling and optimization with AI-assisted planning.",
     href: "https://smart-class-room-woad.vercel.app/",
     image: "/assets/projects/Scheduler.png",
-    bgImage: "public/assets/backgrounds/bg1.jpg",
+    bgImage: "/assets/backgrounds/bg1.jpg",
     frameworks: [
       { id: 1, name: "MERN Stack" },
       { id: 2, name: "Tailwind CSS" },
@@ -36,7 +36,7 @@ export const projects = [
       "A rental marketplace that helps students and professionals find verified rooms and accommodations quickly.",
     href: "https://easemyroom.daapen.com/index.php",
     image: "/assets/projects/easemyroom.png",
-    bgImage: "public/assets/backgrounds/bg1.jpg",
+    bgImage: "/assets/backgrounds/bg1.jpg",
     frameworks: [
       { id: 1, name: "MERN Stack" },
     ],
@@ -48,7 +48,7 @@ export const projects = [
       "A secure real-time messaging application with live conversations, authentication, and scalable socket communication.",
     href: "https://chat-app-gxx1.vercel.app/login",
     image: "/assets/projects/chatapp.png",
-    bgImage: "public/assets/backgrounds/bg1.jpg",
+    bgImage: "/assets/backgrounds/bg1.jpg",
     frameworks: [
       { id: 1, name: "React" },
       { id: 2, name: "Node.js" },
